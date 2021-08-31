e75a1d08599caad758a17f9e5ba35718f9904257
new-store <vadimpronkin19@gmail.com>
2021-08-31 23:01:05 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,35 @@
+const upBtn = document.getElementById('upBtn');
+const downBtn = document.getElementById('downBtn');
+
+const quickTablePart = document.getElementsByClassName('table__top')[0];
+
+upBtn.addEventListener('click', () => {
+    let scaleCoef = quickTablePart.clientWidth / 334.5;
+    quickTablePart.style.transform = `translateY(${20 * scaleCoef}px)`;
+});
+
+downBtn.addEventListener('click', () => {
+    let scaleCoef = quickTablePart.clientWidth / 334.5;
+    quickTablePart.style.transform = `translateY(${70 * scaleCoef}px)`;
+});
+
+
+const windowScroll = document;
+const animatedBox = document.getElementsByClassName('bot')[0];
+const animatedGirl = document.getElementsByClassName('bot__girl')[0];
+const animatedHole = document.getElementsByClassName('bot__hole')[0];
+
+windowScroll.addEventListener('scroll', ()=>{
+    let dist = document.getElementById('start-appearance').getBoundingClientRect().y;
+    console.log(dist)
+
+    if(dist >= -50 && dist <= 650) {
+        animatedBox.style.display = 'flex'
+        animatedGirl.classList.add('animate_appearance');
+        animatedHole.classList.add('animate_resize');
+
+        setTimeout(() => {
+            animatedGirl.classList.add('animate_levitate');
+        }, 3000)
+    }
+});
